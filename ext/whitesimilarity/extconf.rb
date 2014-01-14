36ded3ae060399f52d8f1ef3561c84841fab817c
Jeremy Fairbank <elpapapollo@gmail.com>
2014-01-14 10:38:21 -0500

    Commenting out extra customization for headers and libs because I may have a better solution for including the white similarity library as an object

--- ext/whitesimilarity/extconf.rb
+++ ext/whitesimilarity/extconf.rb
@@ -1,21 +1,22 @@
 require 'mkmf'
 
-WS_LIB_PATH = File.expand_path('../lib', __FILE__)
-LIBDIR      = RbConfig::CONFIG['libdir']
-INCLUDEDIR  = RbConfig::CONFIG['includedir']
-HEADER_DIRS = [INCLUDEDIR]
-LIB_DIRS    = [WS_LIB_PATH, LIBDIR]
+#WS_LIB_PATH = File.expand_path('../lib', __FILE__)
+#LIBDIR      = RbConfig::CONFIG['libdir']
+#INCLUDEDIR  = RbConfig::CONFIG['includedir']
+#HEADER_DIRS = [WS_LIB_PATH, INCLUDEDIR]
+#LIB_DIRS    = [WS_LIB_PATH, LIBDIR]
 
 extension_name = 'whitesimilarity'
 
-dir_config(extension_name, HEADER_DIRS, LIB_DIRS)
+dir_config(extension_name)
+#dir_config(extension_name, HEADER_DIRS, LIB_DIRS)
 
-unless have_header('lib/whitesimilarity.h')
-  abort "Need 'lib/whitesimilarity.h'"
-end
+#unless have_header('whitesimilarity.h')
+#  abort "Need 'whitesimilarity.h'"
+#end
 
-unless have_library('white_similarity', nil)
-  abort "Need 'libwhite_similarity'"
-end
+#unless have_library('white_similarity', nil)
+#  abort "Need 'libwhite_similarity'"
+#end
 
 create_makefile(extension_name)
