5063e8964bb0200718fc6d7ebdb9d11ff38b9f47
Jeremy Fairbank <elpapapollo@gmail.com>
2014-01-14 11:59:44 -0500

    Remove commented out code

--- ext/whitesimilarity/extconf.rb
+++ ext/whitesimilarity/extconf.rb
@@ -1,22 +1,5 @@
 require 'mkmf'
 
-#WS_LIB_PATH = File.expand_path('../lib', __FILE__)
-#LIBDIR      = RbConfig::CONFIG['libdir']
-#INCLUDEDIR  = RbConfig::CONFIG['includedir']
-#HEADER_DIRS = [WS_LIB_PATH, INCLUDEDIR]
-#LIB_DIRS    = [WS_LIB_PATH, LIBDIR]
-
 extension_name = 'whitesimilarity'
-
 dir_config(extension_name)
-#dir_config(extension_name, HEADER_DIRS, LIB_DIRS)
-
-#unless have_header('whitesimilarity.h')
-#  abort "Need 'whitesimilarity.h'"
-#end
-
-#unless have_library('white_similarity', nil)
-#  abort "Need 'libwhite_similarity'"
-#end
-
 create_makefile(extension_name)
